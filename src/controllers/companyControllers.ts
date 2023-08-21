4e427646792ba5f770428acd84e7d00baab52908
Avesh <avesh.webosmotic@gmail.com>
2023-08-21 13:33:40 +0530

    Completed the CRUD for the Company

--- src/controllers/companyControllers.ts
+++ src/controllers/companyControllers.ts
@@ -8,12 +8,22 @@ export const createCompany = async (
   res: Response,
   next: NextFunction
 ) => {
-  const addCompany = new Company(req.body);
-  try {
-    const result = await addCompany.save();
-    res.status(201).send(result);
-  } catch (error) {
-    next(error);
+  const existedCompany = await Company.find({
+    $or: [
+      { name: { $regex: new RegExp(req.body.name, "i") } },
+      { email: { $regex: new RegExp(req.body.email, "i") } },
+    ],
+  });
+  if (!existedCompany.length) {
+    const addCompany = new Company(req.body);
+    try {
+      const result = await addCompany.save();
+      return res.status(201).send(result);
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    next(createError(400, "Same email or name of company is already existed!"));
   }
 };
 
@@ -44,7 +54,7 @@ export const deleteCompany = async (
   const { id } = req.params;
   if (!id) return next(createError(404, "Not Found!"));
   try {
-    await Company.findByIdAndRemove(id, req.body);
+    await Company.findByIdAndRemove(id);
     res.status(200).send("Deleted!");
   } catch (error) {
     next(error);
@@ -52,4 +62,41 @@ export const deleteCompany = async (
 };
 
 //Get company
-export const getCompanies = (req: Request, res: Response) => {};
+export const getCompanies = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    if (Object.keys(req.params).length) {
+      //Params
+      const { id } = req.params;
+      const getCompany = await Company.find({ _id: id });
+      return res.send(getCompany);
+    } else if (Object.keys(req.query).length) {
+      //Search Queries
+      const queryObj = {} as any;
+      for (const key in req.query) {
+        if (req.query[key]) {
+          queryObj[key] = req.query[key];
+        }
+      }
+      //For find the related result from the search query for array of multiple query properties
+      const andQueries = Object.keys(queryObj).map((key) => {
+        const searchQuery = {} as any;
+        if (key === "status") {
+          searchQuery[key] = queryObj[key];
+        } else {
+          searchQuery[key] = { $regex: new RegExp(queryObj[key], "i") };
+        }
+        return searchQuery;
+      });
+      const companies = await Company.find({ $and: andQueries });
+      return res.send(companies);
+    }
+    const allCompanies = await Company.find({});
+    return res.send(allCompanies);
+  } catch (error) {
+    next(error);
+  }
+};
--- src/middlewares/joiValidationMiddlewares.ts
+++ src/middlewares/joiValidationMiddlewares.ts
@@ -4,18 +4,19 @@ import {
   empValidation,
   statusValidation,
 } from "../utils/joiValidations.js";
+import createHttpError from "http-errors";
 
 const validateCompany = (req: Request, res: Response, next: NextFunction) => {
   if (req.method === "PUT") {
     const result = companyValidation.validate(req.body);
     if (result.error) {
-      return res.send(result.error.message);
+      return next(createHttpError(400, result.error.message));
     }
   }
   if (req.method === "PATCH") {
     const result = statusValidation.validate(req.body);
     if (result.error) {
-      return res.send(result.error.message);
+      return next(createHttpError(400, result.error.message));
     }
   }
   next();
